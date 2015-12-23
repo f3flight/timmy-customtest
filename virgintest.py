1c77cbf745ef29d43b64627558625387c3e1b671
Dmitry Sutagin <dsutyagin@mirantis.com>
2015-12-23 21:05:17 +0300

    Implement MU satety check
    Add two functions for package versions checks
    closely resembling the comparison functionality algorithms
    found in rpm and dpkg sources

--- virgintest.py
+++ virgintest.py
@@ -24,6 +24,167 @@ import sqlite3
 import re
 import os
 
+# implementation of RPM's rpmvercmp function
+# http://rpm.org/wiki/PackagerDocs/Dependencies
+# http://rpm.org/api/4.4.2.2/rpmvercmp_8c-source.html#l00015
+def rpm_vercmp(a, b):
+    a_newer = 1
+    b_newer = -1
+    equal = 0
+    if a == b:
+        return equal
+    if a and not b:
+        return a_newer
+    if b and not a:
+        return b_newer
+    a_epoch = re.match('^(-?\d):', a)
+    b_epoch = re.match('^(-?\d):', b)
+    if a_epoch:
+        if b_epoch:
+            if int(a_epoch.groups()[0]) > int(b_epoch.groups()[0]):
+                return a_newer
+            if int(a_epoch.groups()[0]) < int(b_epoch.groups()[0]):
+                return b_newer
+        if int(a_epoch.groups()[0]) > 0:
+            return a_newer
+        if int(a_epoch.groups()[0]) < 0:
+            return b_newer
+    elif b_epoch:
+        if int(b_epoch.groups()[0]) > 0:
+            return b_newer
+        if int(b_epoch.groups()[0]) < 0:
+            return a_newer
+    a_list = re.findall('[a-zA-Z]+|[0-9]+', a)
+    b_list = re.findall('[a-zA-Z]+|[0-9]+', b)
+    for index, value in enumerate(a_list):
+        if index > len(b_list):
+            return a_newer
+        else:
+            if value.isdigit():
+                if not b_list[index].isdigit():
+                    return a_newer
+                else:
+                    if int(value) > int(b_list[index]):
+                        return a_newer
+                    if int(value) < int(b_list[index]):
+                        return b_newer
+            else:
+                if b_list[index].isdigit():
+                    return b_newer
+                else:
+                    if value > b_list[index]:
+                        return a_newer
+                    if value < b_list[index]:
+                        return b_newer
+    if len(b_list) > len(a_list):
+        return b_newer
+    return equal
+
+# implementation of Debian version comparison
+# https://www.debian.org/doc/debian-policy/ch-controlfields.html#s-f-Version
+# http://dpkg.sourcearchive.com/documentation/1.15.6/vercmp_8c-source.html
+def deb_vercmp(a, b):
+
+    def cmp(a, b):
+
+        def order(x):
+            if x == '~':
+                return -1
+            if x.isdigit():
+                return int(x)
+            if ord(x) in range(ord('A'), ord('Z')+1)+range(ord('a'),ord('z')):
+                return x
+            else:
+                return ord(x) + 256
+
+        ia = 0
+        ib = 0 
+        while ia < len(a) or ib < len(b):
+            diff = 0
+            # workaround for end of string, add 0 to compare lower then everything except '~'
+            # it is impossible that both ia and ib get over string bounds, so no endless loop possibility
+            if ia = len(a):
+                a += '0'
+            if ib = len(b):
+                b += '0'
+            while (ia < len(a) and not a[ia].isdigit()) or ( ib < len(b) and not b[ib].isdigit()):
+                if order(a[ia]) > order(b[ib]):
+                    return 1
+                if order(a[ia]) < order(b[ib]):
+                    return -1
+                i_a += 1
+                i_b += 1
+            while ia < len(a) and a[i_a] == '0':
+                i_a += 1
+            while ib < len(b) b[i_b] == '0':
+                i_b += 1
+            while ia < len(a) and a[ia].isdigit() and ib < len(b) and b[ib].isdigit():
+                if not diff:
+                    diff = int(a[ia]) - int(b[ib])
+                    ia += 1
+                    ib += 1
+            if ia < len(a) and a[ia].isdigit():
+                return 1
+            if ib < len(b) and b[ib].isdigit():
+                return -1
+            if diff:
+                return diff
+        return 0
+
+    a_newer = 1
+    b_newer = -1
+    equal = 0
+    if a == b:
+        return equal
+    if a and not b:
+        return a_newer
+    if b and not a:
+        return b_newer
+    a_epoch = re.match('^(\d):', a)
+    b_epoch = re.match('^(\d):', b)
+    if a_epoch:
+        if b_epoch:
+            if int(a_epoch.groups()[0]) > int(b_epoch.groups()[0]):
+                return a_newer
+            if int(a_epoch.groups()[0]) < int(b_epoch.groups()[0]):
+                return b_newer
+        elif int(a_epoch.groups()[0]) > 0:
+            return a_newer
+        else:
+            b = b[2:]
+        a = a[2:]
+    elif b_epoch:
+        if int(b_epoch.groups()[0]) > 0:
+            return b_newer
+        b = b[2:]
+
+    a_parts = re.match('^([^-].+?)?(?:-([^-]+))?$', a)
+    a_version = a_revision = None
+    if a_parts:
+        a_version, a_revision = a_parts.groups()
+    b_parts = re.match('^([^-].+?)?(?:-([^-]+))?$', b)
+    b_version = b_revision = None
+    if b_parts:
+        b_version, b_revision = b_parts.groups()
+    if a_version and not b_version:
+        return a_newer
+    if b_version and not a_version:
+        return b_newer
+    vc = cmp(a_version, b_version)
+    if vc > 0:
+        return a_newer
+    if vc < 0:
+        return b_newer
+    if a_revision and not b_revision:
+        return a_newer
+    if b_revision and not a_revision:
+        return b_newer
+    rc = cmp(a_revision, b_revision)
+    if rc > 0:
+        return a_newer
+    if rc < 0:
+        return b_newer
+    return equal
 
 def load_versions_database(sqlite_db):
     '''
@@ -89,6 +250,7 @@ def verify_versions(versions_db_cursor, nodes, node):
         return
     with open(node.mapcmds[command],'r') as packagelist:
         reader = csv.reader(packagelist, delimiter='\t')
+        node.custom_packages = {}
         for p_name, p_version in reader:
             match = versions_db_cursor.execute('''
                 SELECT * FROM versions
@@ -97,6 +259,7 @@ def verify_versions(versions_db_cursor, nodes, node):
                     AND package_name = ?
                     AND package_version = ?''', (node.release, node.os_platform, p_name, p_version)).fetchall()
             if not match:
+                node.custom_packages[p_name] = p_version
                 # try all releases
                 match = versions_db_cursor.execute('''
                     SELECT * FROM versions
@@ -130,6 +293,7 @@ def verify_versions(versions_db_cursor, nodes, node):
                     +': package not in db - '+p_name
                     +' (installed version - '+str(p_version)+')')
                 continue
+        return custom_packages
 
 def verify_md5_builtin_show_results(nodes, node):
     ignored_packages = [ 'vim-tiny' ]
@@ -171,7 +335,63 @@ def verify_md5_with_db_show_results(nodes, node):
                         +', version '+str(version)
                         +' - '+str(details))
 
+def max_versions_dict(versions_db)
+    versions_db_cursor = versions_db.cursor()
+    data = versions_db_cursor.execute('''
+        SELECT release, os, package_name, package_version
+        FROM versions
+        ''').fetchall()
+    max_version = {}
+    for el in data:
+        if el(0) not in max_version:
+            max_version[el(0)] = {}
+        if el(1) not in max_version[el(0)]:
+            max_version[el(0)][el(1)] = {}
+        if el(2) not in max_version[el(0)][el(1)]:
+            max_version[el(0)][el(1)][el(2)] = el(3)
+        else:
+            if el(1) = 'centos':
+                if rpm_vercmp(el(3), max_version[el(0)][el(1)][el(2)]) > 0:
+                    max_version[el(0)][el(1)][el(2)] = el(3)
+            if el(1) = 'ubuntu':
+                if deb_vercmp(el(3), max_version[el(0)][el(1)][el(2)]) > 0:
+                    max_version[el(0)][el(1)][el(2)] = el(3)
+    return max_version
 
+def mu_safety_check(node, mvd):
+    if node.custom_packages():
+        for p_name, p_version in custom_packages
+            if node.release in mvd:
+                if node.os in mvd[node.release]:
+                    if p_name in mvd[node.release][node.os]:
+                        if node.os = 'centos':
+                            if rpm_vercmp(mvd[node.release][node.os][p_name],p_version) > 0:
+                                print('env '+str(node.cluster)
+                                    +', node '+str(node.node_id)
+                                    +': custom package '+ str(p_name)
+                                    +' version '+str(p_version)
+                                    +' will be overwritten by MU version '+str(mvd[node.release][node.os][p_name]))
+                            else:
+                                print('env '+str(node.cluster)
+                                    +', node '+str(node.node_id)
+                                    +': custom package '+ str(p_name)
+                                    +' version '+str(p_version)
+                                    +' will prevent (MU or release) version '+str(mvd[node.release][node.os][p_name]))
+                                    +'from being installed')
+                        elif node.os = 'ubuntu':
+                            if deb_vercmp(mvd[node.release][node.os][p_name],p_version) > 0:
+                                print('env '+str(node.cluster)
+                                    +', node '+str(node.node_id)
+                                    +': custom package '+ str(p_name)
+                                    +' version '+str(p_version)
+                                    +' will be overwritten by MU version '+str(mvd[node.release][node.os][p_name]))
+                            else:
+                                print('env '+str(node.cluster)
+                                    +', node '+str(node.node_id)
+                                    +': custom package '+ str(p_name)
+                                    +' version '+str(p_version)
+                                    +' will prevent (MU or release) version '+str(mvd[node.release][node.os][p_name]))
+                                    +'from being installed')
 
 def main(argv=None):
     n = nodes_init()
@@ -196,6 +416,11 @@ def main(argv=None):
         if node.status == 'ready':
             verify_md5_with_db_show_results(n, node)
 
+    print('MU safety analysis'):
+    mvd = max_versions_dict(versions_db)
+    for node in n.nodes.values():
+        if nodes.status == 'ready':
+            mu_safety_check(node, mvd)
     return 0
 
 if __name__ == '__main__':
