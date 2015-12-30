88eb3af158eacd11e0707d7d4e0f1f5f99559a97
root <root@fuel.domain.tld>
2015-12-30 15:09:34 +0000

    Update versions db 6.1 ubuntu from online security repo Package file add update script

---
+++ util/update-versions-db-6.1-from-ubuntu-online-mos6.1-security-Packages.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+db='../db/versions/6.1/versions.tsv'
+
+packagesdata="$(wget -O Packages http://mirror.fuel-infra.org/mos/ubuntu/dists/mos6.1-security/main/binary-amd64/Packages)"
+packagesdata="$(cat Packages)"
+
+lastid=$(tail -n 1 $db | cut -f1)
+id=$lastid
+
+echo "$packagesdata" | ./parse-Packages.pl | while read line
+do
+  fn=$(echo "$line" | cut -f3)
+  if [ $(grep -c "$line" $db) -eq 0 ]
+  then
+    if [ $(grep -c "$fn" $db) -eq 0 ]
+    then
+      id=$((id+1))
+      echo -e "$id\t0\t6.1\trelease\tubuntu\t$line" >> $db
+    else
+      for n in $(grep -n "	$fn" $db | cut -d: -f1)
+      do
+        l="$(sed -n "${n}p" $db)"
+        part="$(echo "$l" | cut -f-5)"
+        sed -i "${n}s/.*/${part}\t${line}/" $db
+      done
+    fi
+  fi
+done
+
